32d57817b0c152eb06a81b1c5e1628c20d0133aa
Carlos Torrealba <calrrox@gmail.com>
2021-03-01 18:32:57 -0400

    Init commit

---
+++ lib/client_machine_service.rb
@@ -0,0 +1,9 @@
+require "client_machine_service/version"
+
+module ClientMachineService
+  class Error < StandardError; end
+
+  def self.start
+    # code hete
+  end
+end
---
+++ spec/ruby_db_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ClientMachineService do
+  it "has a version number" do
+    expect(ClientMachineService::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
